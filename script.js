f56d42c5b913b6806a78950f0e4a7320bf1c5cda
Elypt404 <beartor263@proton.me>
2025-05-28 16:57:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // JavaScript used to set randomness for particles.
 // Could be done via SSR
-
+const button = document.querySelector('.sparkle-button button');
+button.addEventListener('click', () => {
+  window.location.href = 'Realindex.html';
+});
 const RANDOM = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const PARTICLES = document.querySelectorAll('.particle');
 PARTICLES.forEach(P => {
@@ -14,8 +17,4 @@ PARTICLES.forEach(P => {
 		--origin-y: ${Math.random() > 0.5 ? RANDOM(300, 800) * -1 : RANDOM(300, 800)}%;
 		--size: ${RANDOM(40, 90) / 100};
 	`);
-});
-const button = document.querySelector('button'); // or use an ID/class selector if multiple buttons
-button.addEventListener('click', () => {
-  window.location.href = 'Realindex.html';
 });
