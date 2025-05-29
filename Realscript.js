c3e21d4d97a3d14e9df18e5fe1df0eeff6f492c3
Elypt404 <beartor263@proton.me>
2025-05-29 19:18:42 +0100

    Update Realscript.js

--- Realscript.js
+++ Realscript.js
@@ -1,3 +1,1 @@
-document.querySelector('button').addEventListener('click', () => {
-  document.documentElement.dataset.debug = document.documentElement.matches('[data-debug="true"]') ? "false" : "true"
-})
+
